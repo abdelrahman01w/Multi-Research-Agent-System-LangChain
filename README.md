1741ab5eec03bd79e6b62229462c0a96b47a6df8
abdelrahman <abdelrahman87c@gmail.com>
2026-09-14 12:22:39 +0300

    add agent tools and protect environment secrets

---
+++ README.md
@@ -0,0 +1,5 @@
+conda create -n multi_agent_research_system python=3.11
+
+conda activate multi_agent_research_system
+
+pip install -r requirements.txt
---
+++ main.py
@@ -0,0 +1,5 @@
+from src.tools.tools import web_serach, scrape_url
+
+r = web_serach.invoke("What is the latest research on quantum computing?")
+
+print(r)
